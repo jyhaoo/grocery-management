1899cfee35c71c84c8a9a304f8fa18f126395a10
Jason Hao <jyhao1998@gmail.com>
2024-03-01 21:12:20 -0800

    feat: initial commit of grocery list

--- grocery-management-reactjs/src/components/groceryList/index.js
+++ grocery-management-reactjs/src/components/groceryList/index.js
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
+const GroceryContainer = styled.div`
+  background-color: blue;
+  width: 968px;
+  height: 450px;
+  margin-top: 210px;
+  margin-left: 71px;
+  margin-right: 95px;
+  border-radius: 25px;
+`;
+
 const GroceryList = () => {
-  return <div>Grocery list</div>;
+  return <GroceryContainer></GroceryContainer>;
 };
 
 export default GroceryList;
